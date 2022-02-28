88a1271ec86c7c84484871ba510b99ce023931f4
codesnack <codesnack@gmail.com>
2022-02-28 10:55:44 +0000

    dsadad

--- config.js
+++ config.js
@@ -3,6 +3,10 @@ module.exports = {
   apiToken: 'BOT_API_TOKEN',
   test: 8,
   x: 1,
+  dsada: 33443433333,
+  rwerwer: 2,
   dsada: 33333,
-  rwerwer: 2
+  dsad2a: 33333,
+  dsa3da: 33333,
+  dsa4da: 33333,
 }
